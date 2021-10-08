4de0ea24c01f5e734cf451b6491da2e1c47ce396
Evgeniy <lysenko765@yandex.ru>
2021-10-08 20:01:07 +0300

    Начальный коммит

---
+++ WarehouseOrganizer/src/main/java/org/pstgu/WarehouseOrganizer/Application/Main.java
@@ -0,0 +1,32 @@
+package org.pstgu.WarehouseOrganizer.Application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			
+			Label text = new Label("Hello PSTGU!");
+			root.setCenter(text);
+			
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
